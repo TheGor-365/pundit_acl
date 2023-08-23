878ee18ec88767a90f5fa1e1cfa84ee4e62d5445
Gor <7495500@gmail.com>
2023-08-23 21:36:52 +0300

    Add Authorization with bcrypt

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by(login: params[:login])
+
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+    else
+      redirect_to user_path(@user)
+    end
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   root "welcome#homepage"
 
-  resources :users, only: [:new, :create, :show]
-
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
+  resources :users,    only: [:new, :create, :show]
+  resources :sessions, only: [:new, :create, :destroy]
 end
